2a79a8032fd1eaf4dfa5c0be0db56315aaf1f93e
Oscar Salinas Mora <oscar.salinas.mora@me.com>
2025-11-06 16:18:52 -0600

    Fin Sección 7

---
+++ src/app/pages/accountsettings/accountsettings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accountsettings',
+  templateUrl: './accountsettings.component.html',
+  styles: [
+  ]
+})
+export class AccountsettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountsettingsComponent } from './accountsettings/accountsettings.component';
 
 
 
@@ -20,12 +21,14 @@ import { PagesComponent } from './pages.component';
         ProgressComponent,
         Grafica1Component,
         PagesComponent,
+        AccountsettingsComponent,
   ],
   exports: [
         DashboardComponent,
         ProgressComponent,
         Grafica1Component,
         PagesComponent,
+        AccountsettingsComponent,
   ],
   imports: [
     CommonModule,
